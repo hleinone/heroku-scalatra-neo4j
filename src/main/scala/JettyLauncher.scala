d7a3891d702a4aa08402462e0b3f56062d3466fa
Hannu Leinonen <hleinone@gmail.com>
2012-02-03 03:54:31 +0200

    Added proper mode setup.

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -8,13 +8,14 @@ import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 
 object JettyLauncher {
   def main(args: Array[String]) {
+    System.setProperty("org.scalatra.environment", "heroku")
     val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
     val server = new Server(port)
     val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
 
     context.addFilter(classOf[ExampleEndpointFilter], "/*", 0)
-    context.addServlet(classOf[DefaultServlet], "/");
+    context.addServlet(classOf[DefaultServlet], "/")
     context.setResourceBase("src/main/webapp")
 
     server.start
