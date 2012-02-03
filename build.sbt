6dac0abfbfd95dcbe4470d5590bf05da3df2cb1b
Hannu Leinonen <hleinone@gmail.com>
2012-02-03 03:11:59 +0200

    Created simple and working Neo4j example.

--- build.sbt
+++ build.sbt
@@ -14,13 +14,15 @@ scalaVersion := "2.9.1"
 libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % "2.0.3",
   "org.scalatra" %% "scalatra-scalate" % "2.0.1",
+  "org.neo4j" % "neo4j-rest-graphdb" % "1.6",
   "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container",
   "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725",
   "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
 )
 
 resolvers ++= Seq(
-  "Sonatype OSS" at "http://oss.sonatype.org/content/repositories/releases/",
+  "Sonatype OSS" at "http://oss.sonatype.org/content/repositories/releases",
   "Web plugin repo" at "http://siasia.github.com/maven2",
-  "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+  "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+  "Neo4j Release Repository" at "http://m2.neo4j.org/content/repositories/releases"
 )
--- src/main/scala/ExampleEndpointFilter.scala
+++ src/main/scala/ExampleEndpointFilter.scala
@@ -1,15 +1,40 @@
 import org.scalatra.ScalatraFilter
 import org.scalatra.scalate.ScalateSupport
 import java.net.URL
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.rest.graphdb.RestGraphDatabase
+import java.net.URI
+import org.neo4j.graphdb.RelationshipType
+import org.neo4j.graphdb.DynamicRelationshipType
+import collection.JavaConversions._
 
 class ExampleEndpointFilter extends ScalatraFilter with ScalateSupport {
+  val gds: GraphDatabaseService =
+    if (isDevelopmentMode)
+      new RestGraphDatabase("http://localhost:7474/db/data")
+    else
+      new RestGraphDatabase("http://749dda205:e272997de@d4b0f9790.hosted.neo4j.org:7027/db/data/", "749dda205", "e272997de")
+
+  val me = gds.getReferenceNode()
+  if (!me.hasProperty("name")) {
+    me.setProperty("name", "I")
+    val you = gds.createNode()
+    you.setProperty("name", "you")
+    me.createRelationshipTo(you, DynamicRelationshipType.withName("love"))
+  }
+
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    val me = gds.getNodeById(0)
+    print(me)
+    val rels = me.getRelationships(DynamicRelationshipType.withName("love"))
+    println(rels.size);
+    val rel = rels.head
+    val you = rel.getEndNode()
+    contentType = "text/html"
+    templateEngine.layout("/WEB-INF/scalate/templates/index.scaml",
+      Map("me" -> me.getProperty("name"),
+        "rel" -> rel.getType().name(),
+        "you" -> you.getProperty("name")));
   }
 
   notFound {
